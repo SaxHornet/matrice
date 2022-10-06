56fb6a64b7e8609fda28d8dab8c41f5fba1c93eb
SaxHornet <7604414+SaxHornet@users.noreply.github.com>
2022-10-06 17:05:43 +0200

    Update code.py

--- code.py
+++ code.py
@@ -8,7 +8,6 @@ print ("Projet 1: calculs matriciels avec la librairie Numpy \n")
 def vecteur_y ():
 	y = np.array([[1],[1],[1],[1],[1],[1],[1],[1]])
 	return y
-
 #=debug
 #print ("Le vecteur y :  \n\n",vecteur_y())
 
@@ -16,7 +15,6 @@ def vecteur_y ():
 def I_8 ():
 	i = np.identity(8,dtype=int)
 	return i
-
 #==debug
 #print ("La matrice identitée dans R⁸: \n\n",I_8())
 
@@ -27,7 +25,6 @@ def matrice_E ():
 	return e
 #===debug
 #print ("La matrice E : \n\n\n",matrice_E())
-
 #matrice X : matrice d'observation
 I_8 = np.identity(8,dtype=int)
 matrice_E = np.zeros((8,8),dtype=int)
@@ -35,7 +32,6 @@ matrice_E[0,-1] = 1
 matrix_x = np.add(I_8, matrice_E)
 #====debug
 #print ("La matrice X : \n\n\n", matrix_x)
-
 print ("- Calcul de beta avec la formule du sujet")
 #============utilisation des fonctions de la librairie numpy pour construire le beta
 Transpose = matrix_x.transpose()
@@ -55,20 +51,23 @@ print ("β =  \n\n\n",beta)
 #print ("Vecteur y : \n\n\n",vecteur_y())
 
 ###programme principal
+def sigma(i,n):
+        x = np.arange(i,n)
+        return np.sum([x+1])
+
 print ("                 8    \n")
 print ("* Question 1:    ∑βi \n")
 print ("                i=1 \n")
 #calcul de sigma de 1 à 8 de beta i
 print ("\n\n")
-print ("La somme est de :",np.sum(vecteur_y))
-
+#print ("La somme est de :",np.sum(vecteur_y))
+print ("La somme est de : \n",sigma(1,9) * vecteur_y)
 #donner le rang de la matrice X^T X - I8
 #calcul de la nouvelle matrice
 #n = nouvelle matrice
 n = np.subtract(Transpose @ matrix_x,I_8)
 print ("X^T X - I8 = matrice n : \n\n",n)
 print ("\n\n")
-
 #calcul du rang de la matrice n
 print ("** Question 2: \n")
 n = matrix_rank(n)
